77317b5b1a97492ec92d7ee22e79d2c2c5a97bfa
Anthony <31727292+AnthonyDeletre@users.noreply.github.com>
2019-12-11 16:00:38 +0100

    First Spring Template

---
+++ src/main/java/com/front/controller/ApplicationController.java
@@ -0,0 +1,15 @@
+package com.front.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApplicationController {
+
+    @GetMapping("/")
+    public String produits(Model model) {
+
+        return "index";
+    }
+}
