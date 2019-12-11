23703884604df620e58f3fb3d7dc52cd2d521094
AntoineBdns <antoine.boudens@gmail.com>
2019-12-11 11:01:04 +0100

    Gestion Utilisateur : requêtes basiques

--- src/main/java/application/repository/UserRepository.java
+++ src/main/java/application/repository/UserRepository.java
@@ -1,9 +1,15 @@
 package application.repository;
 
 import application.entity.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     User findById(long id);
     User findByEmail(String email);
+    List<User> findByNom(String nom);
+    List<User> findAll();
+
 }
--- src/main/java/application/service/UserService.java
+++ src/main/java/application/service/UserService.java
@@ -2,11 +2,14 @@ package application.service;
 
 import application.entity.User;
 import application.repository.UserRepository;
+import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class UserService {
@@ -19,10 +22,27 @@ public class UserService {
     @Autowired
     BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    /**
+     * CRUD des Users
+     * @param user
+     */
     public void saveNewUser(User user) {
         user.setPassword(encodePassword(user));
         userRepository.save(user);
         log.info("saveNewUser() : {}", user.toString());
+    }
+
+    /**
+     * Retourne les informations principales de l'utilisateur
+     * @param u Utilisateur
+     * @return Informations au format JSON
+     */
+    public String getUserInfo(User u){
+        JSONObject json = new JSONObject();
+        json.put("nom",u.getNom());
+        json.put("prenom",u.getPrenom());
+        json.put("email",u.getEmail());
+        return json.toJSONString();
     }
 
     public String encodePassword(User user) {
