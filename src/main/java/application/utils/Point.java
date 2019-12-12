a3b36ed27d42e59b9818beba30d71889fd1ae2f8
M4tthieu <matthieu.pellerin.l2info@gmail.com>
2019-12-12 20:51:53 +0100

    Parenthese manquante dans fonction de calcul, ca buggait dans certains cas

--- src/main/java/application/utils/Point.java
+++ src/main/java/application/utils/Point.java
@@ -40,9 +40,10 @@ public class Point {
      * Calcule l'aire du triangle forme par les 3 points
      */
     public static double getAreaTriangle(Point p1, Point p2, Point p3){
+
         return Math.abs((p1.getX()*(p2.getY()-p3.getY()) +
-                p2.getX()*(p3.getY()-p1.getY() +
-                        p3.getX()*(p1.getY()-p2.getY()))) / 2);
+                        p2.getX()*(p3.getY()-p1.getY()) +
+                        p3.getX()*(p1.getY()-p2.getY())) / 2);
 
     }
 
