4009c5dacfdc5977af1f485118511a740f510239
M4tthieu <matthieu.pellerin.l2info@gmail.com>
2019-12-12 20:14:34 +0100

    Mise a jour de area, ajout fonction verification point dans area

--- src/main/java/application/service/AreaService.java
+++ src/main/java/application/service/AreaService.java
@@ -2,45 +2,34 @@ package application.service;
 
 import application.entity.Area;
 import application.utils.Point;
+import org.json.JSONObject;
 
 public class AreaService {
 
-    public boolean isPointInArea(Area area, Point p){
+
+   public boolean isPointInArea(Area area, Point p){
+
+       JSONObject obj = new JSONObject(area.getCoordinates());
+       Point a = new Point(obj.getDouble("x1") ,obj.getDouble("y1"));
+       Point b = new Point(obj.getDouble("x2") ,obj.getDouble("y2"));
+       Point c = new Point(obj.getDouble("x3") ,obj.getDouble("y3"));
+       Point d = new Point(obj.getDouble("x4") ,obj.getDouble("y4"));
+       // a    d
+       //    p
+       // b    c
+
+       // Si la somme des aires des triangles reliant le point est egale a
+       // l'aire du quadrilatere, alors le point est dans la zone
+       double total_area = Point.getAreaTriangle(a, b, c)
+               + Point.getAreaTriangle(a, d, c);
+
+       double sum_area = Point.getAreaTriangle(a, b, p) + Point.getAreaTriangle(b, c, p)
+               + Point.getAreaTriangle(c, d, p) + Point.getAreaTriangle(d, a, p);
+
+       return (sum_area == total_area);
+   }
 
 
-        double x1 = 0;
-        double y1 = 0;
-        double x2 = 0;
-        double y2 = 0;
-        double x3 = 0;
-        double y3 = 0;
-        double x4 = 0;
-        double y4 = 0;
-        Point a = new Point(x1 ,y1);
-        Point b = new Point(x2 ,y2);
-        Point c = new Point(x3 ,y3);
-        Point d = new Point(x4 ,y4);
-
-        // a(x1,y1)    d(x4,y4)
-        //      m(p.x,p.y)
-        // b(x2,y2)    c(x3,y3)
-
-        double scalar_ap_ab = Point.scalarProduct(a, p, b);
-        double scalar_ab_ab = a.squarePoint(b);
-
-
-        double scalar_ap_ad = Point.scalarProduct(a, p, d);
-        double scalar_ad_ad = a.squarePoint(d);
-
-
-        if( ((0 < scalar_ap_ab) && (scalar_ap_ab < scalar_ab_ab))  &&
-                ((0 < scalar_ap_ad) && (scalar_ap_ad < scalar_ad_ad)))
-            return true;
-        return false;
-
-
-
-    }
 
 
 
--- src/main/java/application/utils/Point.java
+++ src/main/java/application/utils/Point.java
@@ -5,8 +5,8 @@ import java.lang.Math;
 
 public class Point {
 
-    private double x;
-    private double y;
+    private final double x;
+    private final double y;
 
 
     public Point(double x, double y) {
@@ -17,31 +17,32 @@ public class Point {
     public double getX() {
         return x;
     }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
     public double getY() {
         return y;
     }
 
-    public void setY(double y) {
-        this.y = y;
-    }
-//
-//    public double square(){
-//        return Math.pow(this.x, 2) + Math.pow(this.y, 2);
-//    }
-
-    public double squarePoint(Point p){
+    private double squarePoint(Point p){
 
         return (Math.pow((p.getX() - this.x),2) + (Math.pow(p.getY() - this.y,2)));
     }
 
+    /**
+     * Calcule le produit scalaire entre 2 vecteurs composes des points donnees
+     * Exemple AB.AC (A p1, B p2, C p3)
+     */
     public static double scalarProduct(Point p1, Point p2, Point p3){
 
         return 0.5 * (p1.squarePoint(p2) + p1.squarePoint(p3) - p2.squarePoint(p3));
+
+    }
+
+    /**
+     * Calcule l'aire du triangle forme par les 3 points
+     */
+    public static double getAreaTriangle(Point p1, Point p2, Point p3){
+        return Math.abs((p1.getX()*(p2.getY()-p3.getY()) +
+                p2.getX()*(p3.getY()-p1.getY() +
+                        p3.getX()*(p1.getY()-p2.getY()))) / 2);
 
     }
 
