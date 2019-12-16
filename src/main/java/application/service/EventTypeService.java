69520167a8e5260f8471b8603a57cb58316ba463
benjamint <talmant.benjamin@gmail.com>
2019-12-16 15:43:50 +0100

    Ajout de commentaires sur chaque fonction (pour Javadoc)

--- src/main/java/application/service/EventTypeService.java
+++ src/main/java/application/service/EventTypeService.java
@@ -21,30 +21,60 @@ public class EventTypeService {
     @Autowired
     private EventTypeRepository eventTypeRepository;
 
+    /**
+     * Sauvegarde d'un nouveau type d'evenement
+     * @param eventType : le type d'evenement à sauvegarder
+     * @return Le type d'evenement cree, null sinon
+     */
     public EventType saveNewEventType(EventType eventType) {
         log.info("save New EventType() : {}", eventType.toString());
         return this.eventTypeRepository.save(eventType);
     }
 
+    /**
+     * Edite un type d'evenement
+     * @param eventType : le type d'evenement à editer
+     * @return Le type d'evenement modifie, null sinon
+     */
     public EventType editEventType(EventType eventType) {
         log.info("edit EventType() : {}", eventType.toString());
         return this.eventTypeRepository.save(eventType);
     }
 
+
+    /**
+     * Supprime un type d'evenement
+     * @param eventType : le type d'evenement à supprimer
+     * @return True si le type d'evenement a bien ete supprime, false sinon
+     */
     public boolean deleteEventType(EventType eventType) {
         log.info("delete EventType() : {}", eventType.toString());
         this.eventTypeRepository.delete(eventType);
         return this.eventTypeRepository.findByName(eventType.getName()) == null;
     }
 
+    /**
+     *  Recherche un type d'evenement par son id
+     * @param id : id du type d'evenement à trouver
+     * @return Le type d'evenement cherché, ou null
+     */
     public EventType findEventTypeById(int id) {
         return this.eventTypeRepository.findById(id);
     }
 
+    /**
+     * Recherche un type d'evenement par son nom
+     * @param name : nom du type d'evenement à trouver
+     * @return Le type d'evenement cherché, ou null
+     */
     public EventType findEventTypeByName(String name) {
         return this.eventTypeRepository.findByName(name);
     }
 
+    /**
+     * Recherche tous les types d'evenement
+     * @return La liste de tous les types d'evenement
+     */
     public List<EventType> findAllEventTypes() {
         return this.eventTypeRepository.findAll();
     }
