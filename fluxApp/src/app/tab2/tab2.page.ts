df413c6fffdcbc7112b72f5a682f0301267dd7b4
Guillaume Jouglet <jeffguigui@hotmail.fr>
2020-01-04 19:23:46 +0100

    Add files via upload

--- fluxApp/src/app/tab2/tab2.page.ts
+++ fluxApp/src/app/tab2/tab2.page.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { EnvService } from '../services/env.service';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { AlertService } from '../services/alert.service';
+import { Platform, NavController } from '@ionic/angular';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 
 @Component({
   selector: 'app-tab2',
@@ -11,8 +16,14 @@ import { Observable } from 'rxjs';
 
 export class Tab2Page {
   events : Array<any>;
-
-  constructor(private http : HttpClient , private env : EnvService) {
+  token: any
+  pass: any
+  constructor(private platform: Platform,
+  private splashScreen: SplashScreen,
+  private statusBar: StatusBar,
+  private authService: AuthService,
+  private navCtrl: NavController,
+  private http : HttpClient , private env : EnvService, private alertService: AlertService) {
     let requestAPI = env.PROXY_URL + env.API_URL + "event/all";
     http.get(requestAPI , {responseType : "text"}).subscribe(
       (response) => {
@@ -23,7 +34,42 @@ export class Tab2Page {
         console.log(error);
       }
     );
+    this.initializeApp();
+  }
 
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      // Commenting splashScreen Hide, so it won't hide splashScreen before auth check
+      //this.splashScreen.hide();
+      this.token = this.authService.storage.getItem('token');
+      console.log("Token dans tab 2");
+      console.log(this.token);
+      this.pass = this.authService.storage.getItem('password');
+      console.log("Password dans tab 2");
+      console.log(this.pass);
+    });
+  }
+
+  // When logout button is pressed
+  logout() {
+    console.log("inside logout");
+    console.log("Email :");
+    console.log(this.token["__zone_symbol__value"]["email"]);
+    console.log("Password :");
+    console.log(this.pass["__zone_symbol__value"]);
+    this.authService.logout(this.token["__zone_symbol__value"]["email"], this.pass["__zone_symbol__value"]).subscribe(
+      data => {
+        this.alertService.presentToast(data['message']);
+      },
+      error => {
+        console.log("Error");
+        console.log(error);
+      },
+      () => {
+        this.navCtrl.navigateRoot('/welcome');
+      }
+    );
   }
 
 }
